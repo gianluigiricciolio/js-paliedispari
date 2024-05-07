46383e29c7eeb3bb39f338dae7ea51ec3657aab3
gianluigiricciolio <gianluigiricciolio@proton.me>
2024-05-07 15:26:35 +0200

    [UPD]add palindroma

---
+++ Palindroma/main.js
@@ -0,0 +1,36 @@
+'use strict';
+
+// Chiedere all’utente di inserire una parola
+
+const palindroma = prompt('Inserire una frase ');
+console.log(`La parola inserita è ${palindroma}`);
+
+// Creare una funzione per capire se la parola inserita è palindroma
+
+if(isPalindroma(palindroma)) console.log('Palindroma');
+else console.log('Non palindroma');
+
+
+
+
+
+
+
+
+
+
+
+function isPalindroma (parola){
+    let palindroma2="";
+    const buffer = new Array(parola.length);
+
+    for (let count = 0; count < parola.length; count++){
+        buffer[parola.length-1-count]=parola[count];
+    }
+    palindroma2=buffer.toString().replace(/,/g,'');
+    console.log(parola);
+    console.log(palindroma2);
+    if(parola!==palindroma2){
+        return false;
+    } else return true;
+}
